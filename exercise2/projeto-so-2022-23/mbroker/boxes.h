72bacd790f013d43a275efc5fe72bc2c42131ca8
o0DARM0o <107251927+o0DARM0o@users.noreply.github.com>
2023-05-18 22:39:30 +0100

    Delete exercise2/projeto-so-2022-23 directory

--- exercise2/projeto-so-2022-23/mbroker/boxes.h
+++
@@ -1,9 +0,0 @@
-bool does_box_exist(char *box_name);
-
-int create_box(char *box_name);
-
-int write_message(char *box_name, char *message);
-
-int remove_box(char *box_name);
-
-int read_box(char *dest, char * box_name);
